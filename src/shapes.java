c809e431c566986a5083bf56c3117fd358007ffc
Bartek Kowalski <bkowalski1975@gmail.com>
2018-05-22 15:43:54 +0200

    zastosowanie przesłaniania w klasie "shapes"

--- src/shapes.java
+++ src/shapes.java
@@ -2,27 +2,32 @@
 class TwoDShape {
     private double width;
     private double height;
+    private String name;
 
     //konstruktor domyślny
     TwoDShape() {
         width = height = 0.0;
+        name = "none";
     }
 
-    //konstruktor klasy bazowej
-    TwoDShape(double w, double h) {
+    //konstruktor z parametrami
+    TwoDShape(double w, double h, String n) {
         width = w;
         height = h;
+        name = n;
     }
 
-    //konstruktor kolejny
-    TwoDShape(double x) {
+    //tworzy obiekt, którego szerokość = wysokość
+    TwoDShape(double x, String n) {
         width = height = x;
+        name = n;
     }
 
     //konstruktor wykorzystujący obiekt.
     TwoDShape(TwoDShape ob) {
         width = ob.width;
         height = ob.height;
+        name = ob.name;
     }
 
 
@@ -32,8 +37,14 @@ class TwoDShape {
     void setWidth(double w) { width = w;}
     void setHeight(double h) {height = h;}
 
+    String getName() {return name;}
+
     void showDim() {
         System.out.println("Szerokość i wysokość obiektu: " + width + " i " + height);
+    }
+    double area() {         //metoda area() zdefiniowana w klasie TwoDShape
+        System.out.println("area() musi być przesłonięta w klasach pochodnych");
+        return 0.0;
     }
 }
 
@@ -46,9 +57,9 @@ class Triangle extends TwoDShape {
         style = "nieokreślony";
     }
 
-    //konstruktor
+    //konstruktor z parametrami
     Triangle(String s, double w, double h) {
-        super(w, h); // odwołanie do konstruktora klasy bazowej
+        super(w, h, "trójkąt"); // odwołanie do konstruktora klasy bazowej
 
 
         style = s;
@@ -56,10 +67,11 @@ class Triangle extends TwoDShape {
 
     //i konstruktor o jednym parametrze
     Triangle(double x){
-        super(x);
+        super(x, "trójkąt");
         style = "wypełniony";
     }
 
+    //konstruktor z obiektami
     Triangle(Triangle ob) {
         super(ob);
         style = ob.style;
@@ -75,42 +87,67 @@ class Triangle extends TwoDShape {
 
 }
 
-//klasa pochodna klasy triangle
-class colorTriangle extends Triangle {
-    private String color;
 
-    colorTriangle(String c, String s, double w, double h) {
-        super(s, w, h);
-        color = c;
+
+
+
+//klasa pochodna klasy bazowej TwoDShape reprezentująca prostokąty
+
+class Rectangle extends TwoDShape {
+
+    //konstruktor domyślny
+    Rectangle() {
+        super();
     }
 
-    String getColor() { return color;}
-    void showColor() {
-        System.out.println("Kolor: " + color);
+    //konstruktor z parametrami
+    Rectangle(double w, double h) {
+        super(w, h, "prostokąt");
+    }
+
+    //konstruktor o jednym parametrze
+    Rectangle(double x) {
+        super(x, "prostokąt");
+    }
+
+    //konstruktor tworzący obiekt na podstawie innego obiektu
+    Rectangle(Rectangle ob) {
+        super(ob);
+    }
+
+
+    boolean isSquare() {
+        if(getWidth() == getHeight()) return true;
+        return false;
+    }
+
+
+    //przesłonia area() w klase rectangle
+    double area() {
+        return getHeight() * getWidth();
     }
 }
 
+
+
+
 class shapes {
     public static void main(String arg[]) {
-        Triangle t1 = new Triangle("pusty", 8.0, 12.0);
-        Triangle t2 = new Triangle(t1);
+        TwoDShape shapes[] = new TwoDShape[5];
 
+        shapes[0] = new Triangle("pusty", 8.0, 12.0);
+        shapes[1] = new Rectangle(10);
+        shapes[2] = new Rectangle(10, 4);
+        shapes[3] = new Triangle(7.0);
+        shapes[4] = new TwoDShape(10, 20, "ogólny");
 
-        System.out.println("Informacje o t1: ");
-        t1.showStyle();
-        t1.showDim();
+        for(int i=0; i < shapes.length; i++) {
+            System.out.println("Typ obiektu: " + shapes[i].getName());
+            System.out.println("Powierzchnia wynosi " + shapes[i].area());
 
-        System.out.println("Powierzchnia wynosi " + t1.area());
+            System.out.println();
+        }
 
-        System.out.println();
-
-        System.out.println("Informacje o t2: ");
-        t2.showStyle();
-        t2.showDim();
-        
-        System.out.println("Powierzchnia wynosi " + t2.area());
-
-        System.out.println();
 
 
     }
