00acc210d2c314cae52f97c9b4cff3d123dad07a
Bartek Kowalski <bkowalski1975@gmail.com>
2018-05-23 19:56:53 +0200

    import pakietów - poprawiona klasa "Usebook"

--- src/bookpackext/Usebook.java
+++ src/bookpackext/Usebook.java
@@ -1,17 +1,17 @@
 package bookpackext;
+import bookpack.*;
 
-/*
+
 class Usebook {
     public static void main(String arg[]) {
-        bookpack.Book books[] = new bookpack.Book[5];
+        Book books2[] = new Book[5];
 
-        books[0] = new bookpack.Book("książka 1", "autor1", 1978);
-        books[1] = new bookpack.Book("książka 2", "autor 2", 2015);
-        books[2] = new bookpack.Book("książka 3", "autor 3", 2013);
-        books[3] = new bookpack.Book("książka 4", "autor 4", 1999);
-        books[4] = new bookpack.Book("książka 5", "autor 5", 1997);
+        books2[0] = new Book("książka 1", "autor1", 1978);
+        books2[1] = new Book("książka 2", "autor 2", 2015);
+        books2[2] = new Book("książka 3", "autor 3", 2013);
+        books2[3] = new Book("książka 4", "autor 4", 1999);
+        books2[4] = new Book("książka 5", "autor 5", 1997);
 
-        for(int i = 0; i < books.length; i ++) books[i].show();
+        for(int i = 0; i < books2.length; i ++) books2[i].show();
     }
 }
-*/
