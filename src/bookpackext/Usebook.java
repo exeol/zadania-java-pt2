afe979f3cb65c477161135c3c392fedee23cb701
Bartek Kowalski <bkowalski1975@gmail.com>
2018-05-23 16:06:17 +0200

    pakiety, dostępy, public private, protected

---
+++ src/bookpackext/Usebook.java
@@ -0,0 +1,16 @@
+package bookpackext;
+
+
+class Usebook {
+    public static void main(String arg[]) {
+        bookpack.Book books[] = new bookpack.Book[5];
+
+        books[0] = new bookpack.Book("książka 1", "autor1", 1978);
+        books[1] = new bookpack.Book("książka 2", "autor 2", 2015);
+        books[2] = new bookpack.Book("książka 3", "autor 3", 2013);
+        books[3] = new bookpack.Book("książka 4", "autor 4", 1999);
+        books[4] = new bookpack.Book("książka 5", "autor 5", 1997);
+
+        for(int i = 0; i < books.length; i ++) books[i].show();
+    }
+}
