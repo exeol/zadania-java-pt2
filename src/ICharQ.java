993d1e1ad76e4d9e7ad5d05522986f4c41992b77
Bartek Kowalski <bkowalski1975@gmail.com>
2018-05-26 18:01:21 +0200

    indeks kolejki

---
+++ src/ICharQ.java
@@ -0,0 +1,8 @@
+public interface ICharQ {
+
+    //umieszcza znaki w kolejce
+    void put(char ch);
+
+    //pobiera znaki z kolejki
+    char get();
+}
---
+++ src/IQDemo.java
@@ -0,0 +1,14 @@
+class FixedQuee implements ICharQ {
+    private char q[];    //tarblica, która będzie przechowywać elementy kolejki
+    private int putloc, getloc;  //indeksy operacji put i get
+
+    
+
+}
+
+
+
+
+
+public class IQDemo {
+}
