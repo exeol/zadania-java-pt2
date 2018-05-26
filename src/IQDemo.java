f5c4adda0dbd178e13d1b3db5f29a408603eaa91
Bartek Kowalski <bkowalski1975@gmail.com>
2018-05-26 19:39:23 +0200

    kolejka zwiększająca pojemność

--- src/IQDemo.java
+++ src/IQDemo.java
@@ -56,13 +56,51 @@ class CircularQueue implements ICharQ {
 
     //metoda pobierająca znak z kolejki
     public char get() {
-        if(getloc = putloc) {
+        if(getloc == putloc) {
             System.out.println("  -- Kolejka psuta.");
             return (char) 0;
         }
         char ch = q[getloc++];
         if(getloc == q.length) getloc = 0;  //powrót na początek tablicy
         return ch;
+    }
+}
+
+
+//kolejka dynamiczna, zwiększająca wielkość w razie potrzeby
+class DynQueue implements ICharQ {
+    private char q[];
+    private int putloc, getloc;
+
+    //konstruktor tworzący pustą kolejkę o podanym rozmiarze
+    public DynQueue(int size) {
+        q = new char[size];
+        putloc = getloc = 0;
+    }
+
+    public void put(char ch) {
+        if(putloc == q.length) {
+            //zwiększa rozmiar kolejki
+            char t[] = new char[q.length * 2];
+
+            //kopiuje elementy do nowej kolejki
+            for(int i = 0; i < q.length; i++)
+                t[i] = q[i];
+
+            q = t;
+        }
+
+        q[putloc++] = ch;
+    }
+
+
+    public char get() {
+        if(getloc == putloc) {
+            System.out.println("  -- Kolejka pusta.");
+            return (char) = 0;
+        }
+
+        return q[getloc++];
     }
 }
 
